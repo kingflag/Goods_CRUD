9656a64636a612e3b59a80840590b8b94d8582b9
kingflag <1362168862@qq.com>
2017-07-17 16:36:09 +0800

    maven聚合，整合quartz，部署后可定时执行任务

---
+++ goods-core/src/main/java/com/king/demo/goods/corejob/TestJob.java
@@ -0,0 +1,12 @@
+package com.king.demo.goods.corejob;
+
+public class TestJob {
+
+  public TestJob() {
+    System.out.println("beginTestJob");
+  }
+
+  public void run() {
+    System.out.println("Test quartz");
+  }
+}
