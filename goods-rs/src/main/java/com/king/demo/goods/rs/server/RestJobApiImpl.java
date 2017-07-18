6e33cfa9aa9465b2299ac6e68b376d69501ff382
kingflag <1362168862@qq.com>
2017-07-18 09:55:29 +0800

    浏览器输入http://localhost:8080/goods-server/rest，可以看到SOAP server

---
+++ goods-rs/src/main/java/com/king/demo/goods/rs/server/RestJobApiImpl.java
@@ -0,0 +1,18 @@
+package com.king.demo.goods.rs.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import om.king.demo.goods.rs.service.RestJobApi;
+
+public class RestJobApiImpl implements RestJobApi {
+
+  @Override
+  public List<String> test() throws Exception {
+    List<String> result = new ArrayList<String>();
+    result.add("lov");
+    result.add("china");
+    return result;
+  }
+
+}
---
+++ goods-rs/src/main/java/om/king/demo/goods/rs/service/RestJobApi.java
@@ -0,0 +1,22 @@
+package om.king.demo.goods.rs.service;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path(value = "/con")
+@Produces(value = {
+  MediaType.APPLICATION_JSON })
+@Consumes(value = {
+  MediaType.APPLICATION_JSON })
+public interface RestJobApi {
+
+  @GET
+  @Path(value = "/func")
+  public List<String> test() throws Exception;
+
+}
