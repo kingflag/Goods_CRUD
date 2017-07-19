96869452b9ab134ad8f9f22e356255627d64dc9e
kingflag <1362168862@qq.com>
2017-07-19 18:59:27 +0800

    根据配置文件中bena --EntityManagerFactory的名字创建实例，并对数据库进行操作

--- goods-core/src/main/java/com/king/demo/goods/data/dao/impl/GoodsOperateDaoImpl.java
+++ goods-core/src/main/java/com/king/demo/goods/data/dao/impl/GoodsOperateDaoImpl.java
@@ -1,20 +1,43 @@
 package com.king.demo.goods.data.dao.impl;
 
+import java.math.BigDecimal;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.util.Date;
 
-import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.king.demo.goods.data.dao.GoodsOperateDao;
+import com.king.demo.goods.data.domain.Goods;
 
-public class GoodsOperateDaoImpl implements GoodsOperateDao {
+public class GoodsOperateDaoImpl implements GoodsOperateDao, ApplicationContextAware {
+
+  protected ApplicationContext appCtx;
+
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    this.appCtx = applicationContext;
+  }
+
+  /** 获得一个EntityManagerFactory。 */
+  protected EntityManager createEntityManager() throws IllegalStateException {
+    EntityManagerFactory emf = null;
+    try {
+      emf = appCtx.getBean("goods-core.emf", EntityManagerFactory.class);
+    } catch (Exception e) {
+      System.out.println("发生异常");
+    }
+    return emf.createEntityManager();
+  }
 
   private JdbcTemplate jdbcTemplate;
-
 
   public void setDataSource(DataSource dataSource) {
     this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -27,7 +50,21 @@ public class GoodsOperateDaoImpl implements GoodsOperateDao {
   @Override
   public String test() {
 
+    EntityManager em = createEntityManager();
     try {
+      Goods goods = new Goods();
+      goods.setGid(232);
+      goods.setCode("test(((00");
+      goods.setName("myname");
+      goods.setRtlprc( new BigDecimal(Double.toString(69.48)));
+      goods.setLastmodifytime(new Date());
+      goods.setState("lllll");
+      em.getTransaction().begin();
+      goods = em.merge(goods);
+      em.flush();
+      em.getTransaction().commit();
+      //return goods; 
+      System.out.println("goods:"+goods);
       System.out.println("7878");
       DatabaseMetaData md = this.jdbcTemplate.getDataSource().getConnection().getMetaData();
       System.out.println(md.getDatabaseProductName());
